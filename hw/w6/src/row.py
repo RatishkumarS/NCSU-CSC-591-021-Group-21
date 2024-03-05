188e7123bf9a51efc879c4e77ae69d15edd2a7c2
unknown <khandekarshardul@gmail.com>
2024-03-05 03:28:34 -0500

    Added dist, neighbors and keysort functions

--- hw/w6/src/row.py
+++ hw/w6/src/row.py
@@ -5,20 +5,17 @@ from constants import *
 class ROW:
     def __init__(self, t):
         self.cells = t
-        # self.k = constants.the['k']
-
-    @classmethod
-    def new(cls, t):
-        return cls(t)
 
     def like(self, data, n, nHypotheses):
         prior = (len(data.row) + the["k"]) / (n + the["k"] * nHypotheses)
         out = math.log(prior) if prior != 0 else float("-inf")
-        for col in data.cols.x.values():
+
+        for col in data.cols.x:
             v = self.cells[col.at]
             if v != "?":
                 inc = col.like(v, prior)
                 out = out + (math.log(inc) if inc != 0 else float("-inf"))
+
         return math.exp(1) ** out
 
     def likes(self, datas):
@@ -35,15 +32,21 @@ class ROW:
                 most, out = tmp, k
 
         return out, most
+    
+    def neighbors(self, data, rows=None):
+        return self.keysort(rows or data.rows, lambda row: self.dist(row, data))
 
-    def dist(self, other, data):
-        d, n, p = 0, 0, 2
-        for col in data.cols.x:
+    def dist(self, other, data, d=0, n=0, p=None):
+        d, n, p = 0, 0, 2  # Assuming the.p is defined in the scope
+        # print("dist->",other.cells)
+        for col in data.cols.x.values():
             n += 1
-            d += col.dist(self.cells[col.at], other.cells[col.at]) ** p
+            d += math.pow(col.dist(self.cells[col.at], other.cells[col.at]), p)
 
         return (d / n) ** (1 / p)
-
-    def neighbors(self, data, rows=None):
-        rows = rows or data.row
-        return sorted(rows, key=lambda row: self.dist(row, data))
+    
+    def keysort(t, fun):
+        u = [{'x': x, 'y': fun(x)} for x in t]
+        u.sort(key=lambda a: a['y'])
+        v = [xy['x'] for xy in u]
+        return v
