a2afe60de8717ab268720e99e23735373c669cd6
unknown <khandekarshardul@gmail.com>
2024-03-05 03:20:35 -0500

    Changed from dict implementation to list to maintain consistency

--- hw/w6/src/cols.py
+++ hw/w6/src/cols.py
@@ -1,37 +1,28 @@
 from num import NUM
-import re
 from sym import SYM
 
 
+# Create a class and define a constructor
 class COLS:
-    # Create
     def __init__(self, row):
-        x, y, all_cols = {}, {}, []
-        klass, col = None, None
+        # Initialize all of the values of empty list
+        self.x, self.y, self.all_cols = [], [], []
+        # Get the names of row cells
+        self.klass, self.names = None, row.cells
 
-        for at, txt in enumerate(row):
-            col = None
-            if re.search("^[A-Z]", txt):
-                col = NUM(txt, at)
-            else:
-                col = SYM(txt, at)
-            all_cols.append(col)
-
+        for at, txt in enumerate(row.cells):
+            # Check whether the col value belongs to NUM or SYM
+            col = (NUM if txt[0].isalpha() and txt[0].isupper() else SYM)(txt, at)
+            self.all_cols.append(col)
             if not txt.endswith("X"):
                 if txt.endswith("!"):
-                    klass = col
-
-                (y if txt.endswith("[!+−]") else x)[at] = col
-
-        self.x = x
-        self.y = y
-        self.all = all_cols
-        self.klass = klass
-        self.names = row
+                    self.klass = col
+                (self.y if txt.endswith(("!", "-", "+")) else self.x).append(col)
 
     # Update
     def add(self, row):
+        # Iterate through all value
         for cols in [self.x, self.y]:
-            for col in cols.values():
+            for col in cols:
                 col.add(row.cells[col.at])
         return row
